80a6cd1463d656c4aed20d6d0b90f5b66720b103
Grizzly <grizzlyxyz@gmail>
2019-01-10 14:36:19 +0100

    exo4

---
+++ style.js
@@ -0,0 +1,6 @@
+// Exo 04 - Combiner les functions partie B
+// Avec les trois variables si dessous et de l'aide des functions, obtenez le résultat suivant :
+// Les chiens sont les meilleurs amis de l'homme"
+let chiens = "les chiens";
+let humains = "on sait de l'homme qu'il est un animal"
+let amis = "sont les meilleurs amis"
